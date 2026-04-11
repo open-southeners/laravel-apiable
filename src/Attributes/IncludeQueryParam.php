7a589db712a5b61d260a9265db0e1c764edcc9a6
Ruben R <ruben@opensoutheners.com>
2026-04-11 20:15:58 +0200

    add $description property to all *QueryParam attributes

--- src/Attributes/IncludeQueryParam.php
+++ src/Attributes/IncludeQueryParam.php
@@ -7,7 +7,7 @@ use Attribute;
 #[Attribute(Attribute::TARGET_CLASS | Attribute::TARGET_METHOD | Attribute::IS_REPEATABLE)]
 final class IncludeQueryParam extends QueryParam
 {
-    public function __construct(public string|array $relationships)
+    public function __construct(public string|array $relationships, public string $description = '')
     {
         //
     }
--- src/Attributes/SearchFilterQueryParam.php
+++ src/Attributes/SearchFilterQueryParam.php
@@ -7,7 +7,7 @@ use Attribute;
 #[Attribute(Attribute::TARGET_CLASS | Attribute::TARGET_METHOD)]
 final class SearchFilterQueryParam extends QueryParam
 {
-    public function __construct(public string $attribute, public $values = '*')
+    public function __construct(public string $attribute, public $values = '*', public string $description = '')
     {
         //
     }
